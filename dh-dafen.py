c96bdf808a01cbd78ed3d0362e2915276e63c321
DH <836705748@qq.com>
2018-04-09 12:20:11 +0800

    huiwen

---
+++ dh-dafen.py
@@ -0,0 +1,11 @@
+def is_palindrome(s):
+    end = len(s) - 1
+    i = 0
+
+    while (s[i] != s[end]):
+        if s[i] != s[end]:
+            return False
+        i += 1
+        end -= 1
+
+    return True 
