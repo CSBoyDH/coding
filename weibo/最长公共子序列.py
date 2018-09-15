a15c425f0805b717cdb20c1c6b97258a373a63b7
DH <836705748@qq.com>
2018-09-15 21:43:19 +0800

    TEST

--- weibo/最长公共子序列.py
+++ weibo/最长公共子序列.py
@@ -1,17 +1,23 @@
 X = input()
 Y  = input()
-def find_lcsubstr(s1, s2):
-    m=[[0 for i in range(len(s2)+1)]  for j in range(len(s1)+1)]  #生成0矩阵，为方便后续计算，比字符串长度多了一列
-    mmax=0   #最长匹配的长度
-    p=0  #最长匹配对应在s1中的最后一位
-    for i in range(len(s1)):
-        for j in range(len(s2)):
-            if s1[i]==s2[j]:
-                m[i+1][j+1]=m[i][j]+1
-                if m[i+1][j+1]>mmax:
-                    mmax=m[i+1][j+1]
-                    p=i+1
-    return s1[p-mmax:p],mmax   #返回最长子串及其长度
-print(find_lcsubstr(X,Y))
-abcd1e1f1
-bcd2e2f2
+
+def get_lcs(string1, string2):
+    '''
+    输入：待比较的两个字符串
+    输出：降序输出的（子序列长度，子序列）列表
+    '''
+    string1_list=list(string1)
+    string2_list=list(string2)
+    lcs_list=[]
+    for i in range(len(string1_list)):
+        flag=0
+        lcs=''
+        for j in range(i,len(string1_list)):
+            for k in range(flag, len(string2_list)):
+                if string1_list[j]==string2_list[k]:
+                    lcs+=string1_list[j]
+                    flag=k+1
+        lcs_list.append((len(lcs), lcs))
+    return sorted(lcs_list, reverse=True)
+
+print(len(get_lcs(X,Y)[0][1]))
